ab4ca932c6e860924fe83e9cb01c15a97b8bb651
Krzysztof Flisikowski <kflisikowski@10g.pl>
2022-12-15 08:00:55 +0100

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,32 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
+
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const remainingDays = document.querySelector('[data-days]');
+const remainingHours = document.querySelector('[data-hours]');
+const remainingMinutes = document.querySelector('[data-minutes]');
+const remainingSeconds = document.querySelector('[data-seconds]');
+let remainingTime = undefined;
+let chosenTime = undefined;
+let timerId = null;
+
+const convertMs = ms => {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+};
